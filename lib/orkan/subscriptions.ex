442d46627a3a69b24c6e9e4e41349c23711dffe8
Arkadiusz Plichta <arkadiusz@blockfi.com>
2021-09-19 12:34:26 +0200

    added proper order

--- lib/orkan/subscriptions.ex
+++ lib/orkan/subscriptions.ex
@@ -14,6 +14,11 @@ defmodule Orkan.Subscriptions do
   end
 
   defp send(user) do
+    user
+    |> get_forecasts()
+  end
+
+  def get_forecasts(user) do
     today = DateTime.new!(Date.utc_today(), ~T[00:00:00.000], "Etc/UTC")
     tomorrow = add_days(today, 1)
     in_two_days = add_days(tomorrow, 2)
@@ -26,29 +31,24 @@ defmodule Orkan.Subscriptions do
         on: f.place_id == p.id,
         select: {p.name, f.datetime, f.wind_speed, f.wind_direction},
         where: s.user_id == ^user.id and f.datetime >= ^tomorrow and f.datetime < ^in_two_days,
-        order_by: p.name
+        order_by: [p.name, f.datetime]
 
-    forecasts =
-      Repo.all(query)
-      |> Enum.group_by(fn {name, _, _, _} -> name end)
-      |> Enum.map(fn {name, forecasts} ->
-        %{
-          place: name,
-          forecasts:
-            Enum.map(forecasts, fn {_, datetime, wind_speed, wind_direction} ->
-              %{
-                datetime: datetime,
-                wind_speed: wind_speed,
-                wind_direction: wind_direction
-              }
-            end)
-        }
-      end)
-      |> IO.inspect()
-
-    # create email
-    # send it
-    forecasts
+    Repo.all(query)
+    |> Enum.group_by(fn {name, _, _, _} -> name end)
+    |> Enum.map(fn {name, forecasts} ->
+      %{
+        place: name,
+        forecasts:
+          Enum.map(forecasts, fn {_, datetime, wind_speed, wind_direction} ->
+            %{
+              datetime: datetime,
+              wind_speed: wind_speed,
+              wind_direction: wind_direction
+            }
+          end)
+      }
+    end)
+    |> IO.inspect()
   end
 
   defp add_days(datetime, days) do
