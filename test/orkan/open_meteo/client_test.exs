e129a010743e4a2e4fe32d7aa9a29abd548ccaea
Arkadiusz Plichta <arkadiusz@blockfi.com>
2022-04-15 20:01:33 +0200

    better client integration test

--- test/orkan/open_meteo/client_test.exs
+++ test/orkan/open_meteo/client_test.exs
@@ -4,8 +4,24 @@ defmodule Orkan.OpenMeteo.ClientTest do
   alias Orkan.OpenMeteo.Client
 
   test "gets data" do
-    data = Client.get_data("", "")
+    data = Client.get_data("54.7147", "18.5581")
 
-    assert %{} = data
+    assert %{
+             "elevation" => _,
+             "generationtime_ms" => _,
+             "hourly" => %{
+               "time" => _,
+               "winddirection_10m" => _,
+               "windspeed_10m" => _
+             },
+             "hourly_units" => %{
+               "time" => "iso8601",
+               "winddirection_10m" => "°",
+               "windspeed_10m" => "km/h"
+             },
+             "latitude" => _,
+             "longitude" => _,
+             "utc_offset_seconds" => 0
+           } = data
   end
 end
